cfd6748e8fd6ea6c07ad59e705fe8e0c8778a765
Pedro Bianchi <pedroedm@gmail.com>
2022-07-26 15:07:38 -0300

    tentativa de implementar a validação do token

--- src/Middlewares/tokenMiddleware.js
+++ src/Middlewares/tokenMiddleware.js
@@ -1,22 +1,17 @@
 const jwt = require('jsonwebtoken');
-const Joi = require('joi');
+require('dotenv').config();
 
-const SECRET = process.env.JWT_SECRET || 'secret';
-
-const tokenMiddleware = () => async (req, res, next) => {
-  try {
-      const token = req.headers.authorization;
-      const schema = Joi.string().required();
-      const validaToken = schema.validate(token);
-
-      if (!token || validaToken.error) {
+const secret = process.env.JWT_SECRET;
+module.exports = async (req, res, next) => {
+    const token = req.headers.authorization;
+    if (!token) {
         return res.status(401).json({ message: 'Token not found' });
-      } 
-      jwt.verify(token, SECRET);
-  } catch (error) {
-      return res.status(401).json({ message: 'Expired or invalid token' });
-  }
-  next();
+    }
+    try {
+        const decoded = jwt.verify(token, secret);
+        req.user = decoded;
+        return next();
+    } catch (e) {
+        return res.status(401).json({ message: 'Expired or invalid token' });
+    }
 };
-
-module.exports = tokenMiddleware;
--- src/Routes/categoriesRoute.js
+++ src/Routes/categoriesRoute.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 const categoryController = require('../Controllers/categoryController');
+const tokenMiddleware = require('../Middlewares/tokenMiddleware');
 
 const categoryRoute = Router();
 
-categoryRoute.post('/', categoryController.addCategory);
-categoryRoute.get('/', categoryController.getCategories);
+categoryRoute.post('/', tokenMiddleware, categoryController.addCategory);
+categoryRoute.get('/', tokenMiddleware, categoryController.getCategories);
 
 module.exports = categoryRoute;
