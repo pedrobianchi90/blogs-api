5c8bad68ec46f58e7394e0b26904ba7ec40399cc
pedrobianchi90 <pedroedm@gmail.com>
2023-09-13 09:15:54 -0300

    Create routes

--- src/Routes/postRoute.js
+++ src/Routes/postRoute.js
@@ -5,6 +5,8 @@ const tokenMiddleware = require('../Middlewares/tokenMiddleware');
 const postRoute = Router();
 
 // postRoute.put('/:id', tokenMiddleware, postController.updatePost);
+// postRoute.put('/:id', tokenMiddleware, postController.deletePost);
+// postRoute.put('/:id', tokenMiddleware, postController.createPost);
 postRoute.get('/', tokenMiddleware, postController.getPosts);
 postRoute.get('/:id', tokenMiddleware, postController.getPostById);
 
